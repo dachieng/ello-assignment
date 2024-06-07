601a0b33f91303988498654b1cb2d8a64c6bbb50
dachieng <oloodorcas99@gmail.com>
2024-06-07 10:40:40 +0300

    chore: initial project setup

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+/* eslint-disable quotes */
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
+import { resolvers } from "./resolvers";
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+(async () => {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4001 },
+  });
+  console.log(`🚀  Server ready at: ${url}`);
+})();
